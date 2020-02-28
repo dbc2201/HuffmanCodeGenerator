cb8a96a105d02030a5d9a8d1e4107b1ceeec17e0
dbc2201 <divyanshb22@gmail.com>
2020-02-28 16:15:48 +0530

    add class structure for code

---
+++ src/huffmanGenerator/HuffmanGenerator.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 28/02/20
+ *   Time: 4:15 PM
+ */
+
+package huffmanGenerator;
+
+public class HuffmanGenerator {
+}
